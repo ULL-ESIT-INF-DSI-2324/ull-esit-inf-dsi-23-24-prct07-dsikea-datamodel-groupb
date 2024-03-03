b6543453d3d9d79be5d402ff1a4f0845350b3b7a
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-03-03 11:35:03 +0000

    Initial Structure

---
+++ tests/test.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, beforeEach } from 'mocha';
+import { expect } from "chai";
+
+
+
+describe('Test of class Equipment', () => {
+  beforeEach(() => {
+  });
+
+  it('Useful test', () =>{
+    expect(true).to.be.equal(false);
+  })
+
+});
